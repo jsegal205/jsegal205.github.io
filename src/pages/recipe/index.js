865f31c8de2cf52a2c07ee11a1139a5fda93de4a
Jim Segal <hello@jimsegal.com>
2020-03-01 19:51:07 -0600

    mock out recipe

--- src/pages/recipe/index.js
+++ src/pages/recipe/index.js
@@ -3,10 +3,31 @@ import "../../App.css";
 
 import { useParams } from "react-router-dom";
 
+const data = {
+  title: "jimtitle",
+  slug: "jimslug",
+  ingredients: "jimingredients",
+  description: "jimdescription",
+  referenceUrl: "http://example.com"
+};
+
 const Recipe = () => {
   const { recipeSlug } = useParams();
 
-  return <h1>Recipe SHOW - {recipeSlug}</h1>;
+  return (
+    <main>
+      <section>
+        <h1>title - {data.title}</h1>
+        <small>
+          <a href={data.referenceUrl}>Original Reference</a>
+        </small>
+      </section>
+      <div>routeSlug - {recipeSlug}</div>
+      <div>slug - {data.slug}</div>
+      <div>ingredients - {data.ingredients}</div>
+      <div>description - {data.description}</div>
+    </main>
+  );
 };
 
 export default Recipe;
