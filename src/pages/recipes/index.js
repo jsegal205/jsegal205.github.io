e39ca1c9e932db3d0dc73dc2e565156858f6c6df
Jim Segal <hello@jimsegal.com>
2020-03-21 14:12:59 -0500

    add search input, hooks and styles

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { apiUrlBase } from "../../utils";
 import useFetch from "../../utils/useFetch";
 import "../../App.css";
+import "./recipes.css";
 
 const Recipes = () => {
   const { loading, data: recipes } = useFetch(`${apiUrlBase}/recipes`);
+
+  const [recipeSearch, setRecipeSearch] = useState("");
+  const handleSearchChange = event => {
+    setRecipeSearch(event.target.value);
+  };
+
+  const [recipeList, setRecipeList] = useState(recipes || []);
+  useEffect(() => {
+    if (!loading) {
+      const results = recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(recipeSearch.toLowerCase())
+      );
+      setRecipeList(results);
+    }
+  }, [loading, recipes, recipeSearch]);
 
   return (
     <section>
       <h2>Recipes</h2>
       {loading && <section>Loading...</section>}
       {recipes && recipes.length && (
-        <ul>
-          {recipes.map(recipe => (
-            <li key={recipe.slug}>
-              <Link
-                to={{
-                  pathname: `/recipe/${recipe.slug}`,
-                  state: { ...recipe }
-                }}
-              >
-                {recipe.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <>
+          <section className="recipes-filter-container">
+            <label htmlFor="recipes-filter">Search</label>
+            <input
+              type="text"
+              id="recipes-filter"
+              value={recipeSearch}
+              onChange={handleSearchChange}
+            />
+          </section>
+          {recipeList.length ? (
+            <ul>
+              {recipeList.map(recipe => (
+                <li key={recipe.slug}>
+                  <Link
+                    to={{
+                      pathname: `/recipe/${recipe.slug}`,
+                      state: { ...recipe }
+                    }}
+                  >
+                    {recipe.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="recipes-no-results">
+              No results for{" "}
+              <strong>
+                <em>{recipeSearch}</em>
+              </strong>
+            </div>
+          )}
+        </>
       )}
     </section>
   );
