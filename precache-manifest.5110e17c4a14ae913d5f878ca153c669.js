769d2b491b2140592912899baf055a58ff84f7ef
Jim Segal <hello@jimsegal.com>
2020-06-09 21:31:21 -0500

    Updates

---
+++ precache-manifest.5110e17c4a14ae913d5f878ca153c669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7569156d03a8e674a30af3300a1989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "976be76a51658eb06346",
+    "url": "/static/css/main.dd672608.chunk.css"
+  },
+  {
+    "revision": "1f64d72fb3ac65ab266d",
+    "url": "/static/js/2.335542da.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.335542da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976be76a51658eb06346",
+    "url": "/static/js/main.3f5e7688.chunk.js"
+  },
+  {
+    "revision": "130773f750f1907c5223",
+    "url": "/static/js/runtime-main.dfc38885.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.705108be909166645e17056b2e00cfe6.js"
+  "/precache-manifest.5110e17c4a14ae913d5f878ca153c669.js"
 );
 
 self.addEventListener('message', (event) => {
