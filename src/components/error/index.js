6085f379dfecdc1f9aac6abbd1874bc73462f2fb
Jim Segal <hello@jimsegal.com>
2020-06-30 21:12:09 -0500

    correct spacing

--- src/components/error/index.js
+++ src/components/error/index.js
@@ -3,8 +3,8 @@ import React from "react";
 const Error = ({ componentName }) => (
   <section>
     <h3>
-      Whoops! There was a problem loading
-      {componentName ? ` ${componentName} data.` : " data."}
+      Whoops! There was a problem loading{" "}
+      {componentName ? `${componentName} data.` : "data."}
     </h3>
     <p>Please reload browser to try again in a little bit.</p>
   </section>
