1e45c39b32321a9e9c682798352dfdf1984158c4
Jim Segal <hello@jimsegal.com>
2020-08-02 15:21:45 -0500

    add project info array of objects

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,7 +1,18 @@
-import { apiUrlBase } from "./index";
+import { apiUrlBase, projectInfo } from "./index";
 
 describe("UTILS - apiUrlBase", () => {
   it("returns prod string", () => {
     expect(apiUrlBase).toEqual("https://api.jimsegal.com");
+  });
+});
+
+describe("UTILS - projectInfo", () => {
+  it("returns array of project info", () => {
+    expect(projectInfo.length).toEqual(3);
+    expect(projectInfo.map((project) => project.title)).toEqual([
+      "Congress",
+      "Recipes",
+      "Space X",
+    ]);
   });
 });
