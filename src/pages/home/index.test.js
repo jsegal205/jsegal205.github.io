930d2dfee7290846b8dd113d5febba43ec7fc599
Jim Segal <hello@jimsegal.com>
2020-08-02 15:59:37 -0500

    mock color values to stop snaps from changing due to random value

--- src/pages/home/index.test.js
+++ src/pages/home/index.test.js
@@ -2,11 +2,27 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render, cleanup } from "@testing-library/react";
 import Home from "./index";
+import {
+  complementaryColor,
+  randomHexColor,
+  textColor,
+} from "../../utils/color";
 
 afterEach(cleanup);
 
+jest.mock("../../utils/color", () => {
+  return {
+    complementaryColor: jest.fn(),
+    randomHexColor: jest.fn(),
+    textColor: jest.fn(),
+  };
+});
+
 describe("Home Component", () => {
   it("renders correctly", () => {
+    complementaryColor.mockReturnValue("000000");
+    randomHexColor.mockReturnValue("ffffff");
+    textColor.mockReturnValue("444444");
     const { container } = render(
       <Router>
         <Home />
