b307c12533921ca0392dca61df73e319dc765a1e
Jim Segal <hello@jimsegal.com>
2020-08-19 20:08:38 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc8e313aeeaedfd5b7fe71b824289b03.js"
+  "/precache-manifest.98b8a0a1481a235a7d30321b804c5621.js"
 );
 
 self.addEventListener('message', (event) => {
