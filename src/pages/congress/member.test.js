cf404fe705f86930e96eb36d2bc8ae3866454222
Jim Segal <hello@jimsegal.com>
2020-09-07 13:47:52 -0500

    add misconduct snap test

--- src/pages/congress/member.test.js
+++ src/pages/congress/member.test.js
@@ -165,6 +165,77 @@ describe("Member Component", () => {
         );
         expect(tree.toJSON()).toMatchSnapshot();
       });
+
+      it("displays misconduct info if present", () => {
+        const chamber = "house";
+        const id = "1";
+        const props = {
+          location: {
+            pathname: `/congress/${chamber}/member/${id}`,
+          },
+        };
+
+        useFetch.mockReturnValue({
+          loading: false,
+          data: {
+            first_name: "first",
+            last_name: "last",
+            most_recent_vote: "most_recent_vote",
+            age: 1,
+            date_of_birth: "1900-01-01",
+            gender: "F",
+            current_party: "D",
+            url: "www.example.com",
+            twitter_account: "twitter",
+            state: "IL",
+            next_election: "2020",
+            terms: 1,
+            initial_elected_in: "2000-01-01",
+            most_recent_vote: "2020-01-01",
+            careerVoting: {
+              careerVotesEligible: 1,
+              careerVotesCast: 2,
+              careerMissedVotes: 3,
+              careerPresentVotes: 4,
+              careerVotesWithParty: 5,
+              careerVotesAgainstParty: 6,
+            },
+            termInfo: [
+              {
+                congress: "116",
+                start_date: "2019-01-03",
+                end_date: "2021-01-03",
+                total_votes: 6,
+                missed_votes: 2,
+                total_present: 3,
+                votesWithParty: 2,
+                votesAgainstParty: 3,
+                party: "D",
+              },
+            ],
+            misconduct: [
+              {
+                allegation: "allegation text",
+                currentStatus: "unresolved",
+                first_date: "2020-01-01",
+                allegationCategories: ["corruption", "elections"],
+                text: "text in [markdown](#markdown)",
+              },
+            ],
+            status: 200,
+          },
+        });
+
+        const tree = renderer.create(
+          <Router>
+            <Member {...props} />
+          </Router>
+        );
+        expect(useFetch).toHaveBeenCalledWith(
+          `${apiUrlBase}/congress/${chamber}/member/${id}`
+        );
+        expect(tree.toJSON()).toMatchSnapshot();
+      });
     });
   });
 
