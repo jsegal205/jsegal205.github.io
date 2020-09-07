6c436d988d35c2829af09bbdd52981814be49d7a
Jim Segal <hello@jimsegal.com>
2020-09-07 13:50:31 -0500

    breakout term party into individual snap test

--- src/pages/congress/member.test.js
+++ src/pages/congress/member.test.js
@@ -138,17 +138,6 @@ describe("Member Component", () => {
                 votesAgainstParty: 3,
                 party: "D",
               },
-              {
-                congress: "115",
-                start_date: "2017-01-03",
-                end_date: "2019-01-03",
-                total_votes: 6,
-                missed_votes: 2,
-                total_present: 3,
-                votesWithParty: 2,
-                votesAgainstParty: 3,
-                party: "R",
-              },
             ],
             misconduct: [],
             status: 200,
@@ -222,6 +211,80 @@ describe("Member Component", () => {
                 text: "text in [markdown](#markdown)",
               },
             ],
+            status: 200,
+          },
+        });
+
+        const tree = renderer.create(
+          <Router>
+            <Member {...props} />
+          </Router>
+        );
+        expect(useFetch).toHaveBeenCalledWith(
+          `${apiUrlBase}/congress/${chamber}/member/${id}`
+        );
+        expect(tree.toJSON()).toMatchSnapshot();
+      });
+
+      it("displays term party if different from current party", () => {
+        const chamber = "house";
+        const id = "1";
+        const props = {
+          location: {
+            pathname: `/congress/${chamber}/member/${id}`,
+          },
+        };
+
+        useFetch.mockReturnValue({
+          loading: false,
+          data: {
+            first_name: "first",
+            last_name: "last",
+            most_recent_vote: "most_recent_vote",
+            age: 1,
+            date_of_birth: "1900-01-01",
+            gender: "F",
+            current_party: "D",
+            url: "www.example.com",
+            twitter_account: "twitter",
+            state: "IL",
+            next_election: "2020",
+            terms: 1,
+            initial_elected_in: "2000-01-01",
+            most_recent_vote: "2020-01-01",
+            careerVoting: {
+              careerVotesEligible: 1,
+              careerVotesCast: 2,
+              careerMissedVotes: 3,
+              careerPresentVotes: 4,
+              careerVotesWithParty: 5,
+              careerVotesAgainstParty: 6,
+            },
+            termInfo: [
+              {
+                congress: "116",
+                start_date: "2019-01-03",
+                end_date: "2021-01-03",
+                total_votes: 6,
+                missed_votes: 2,
+                total_present: 3,
+                votesWithParty: 2,
+                votesAgainstParty: 3,
+                party: "D",
+              },
+              {
+                congress: "115",
+                start_date: "2017-01-03",
+                end_date: "2019-01-03",
+                total_votes: 6,
+                missed_votes: 2,
+                total_present: 3,
+                votesWithParty: 2,
+                votesAgainstParty: 3,
+                party: "R",
+              },
+            ],
+            misconduct: [],
             status: 200,
           },
         });
