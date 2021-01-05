a3cb841c8c7be22e365ea6288408181635d654d6
Jim Segal <hello@jimsegal.com>
2021-01-05 09:24:25 -0600

    add active dev banner

--- src/pages/mountian-goat/index.js
+++ src/pages/mountian-goat/index.js
@@ -585,16 +585,22 @@ const MountainGoat = () => {
         >
           Mountain Goats by Board Game Tables
         </a>
-        . If you have any feedback, find any issues or have more ideas for the
-        game, please reach out to me at{" "}
-        <a
-          href="mailto:projects@jimsegal.com?subject=Mountain Goat Game Feedback!"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          projects@jimsegal.com
-        </a>
-        . Enjoy!
+        .
+        <hr />
+        <strong>
+          This is still in active development and are likely to be issues (like
+          styling on phone, try landscape mode). If you have any feedback, find
+          any issues or have more ideas for the game, please reach out to me at{" "}
+          <a
+            href="mailto:projects@jimsegal.com?subject=Mountain Goat Game Feedback!"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            projects@jimsegal.com
+          </a>
+          . Enjoy!
+        </strong>
+        <hr />
       </section>
       {/*
       <section>
