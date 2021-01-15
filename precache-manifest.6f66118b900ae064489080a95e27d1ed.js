6cf818ee2618d4f36f67ac987df48de35d7bf02e
jsegal205 <hello@jimsegal.com>
2021-01-15 22:07:02 +0000

    Updates

--- precache-manifest.6f66118b900ae064489080a95e27d1ed.js
+++ precache-manifest.6f66118b900ae064489080a95e27d1ed.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9717923c7120962568bd11dfdf027ad0",
+    "revision": "6ccc99be08d84f233f41bdbef3771c4f",
     "url": "/index.html"
   },
   {
-    "revision": "b617c13c6f04e69108ec",
+    "revision": "94da40155d56ebcd3443",
     "url": "/static/css/main.4c2c8a5c.chunk.css"
   },
   {
-    "revision": "fe56cc127233e261fda9",
-    "url": "/static/js/2.4c171352.chunk.js"
+    "revision": "c80e12ef4157b87c81f6",
+    "url": "/static/js/2.b06d8035.chunk.js"
   },
   {
     "revision": "e164c8def4944aad252276b88f6ce1e1",
-    "url": "/static/js/2.4c171352.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b06d8035.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b617c13c6f04e69108ec",
-    "url": "/static/js/main.2b910b29.chunk.js"
+    "revision": "94da40155d56ebcd3443",
+    "url": "/static/js/main.d9d51b47.chunk.js"
   },
   {
     "revision": "b4dcb9066fbdbf137b9a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4da9902ad09631212fe5358c5ae4695.js"
+  "/precache-manifest.6f66118b900ae064489080a95e27d1ed.js"
 );
 
 self.addEventListener('message', (event) => {
