f2a05dbda67023eb6bde2aa3bbb610e898cf226e
Jim Segal <hello@jimsegal.com>
2021-02-15 21:14:04 -0600

    consolidate all topnav items to one

--- src/components/topnav/index.js
+++ src/components/topnav/index.js
@@ -2,26 +2,17 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
 import "./topnav.css";
-import { projectInfo } from "../../utils";
 
 const TopNav = (props) =>
   props.location.pathname === "/" ? null : (
     <>
       <nav>
         <ul className="topnav-list">
-          {projectInfo.map((project) => (
-            <li className="topnav-list-item" key={project.title}>
-              {project.absolute ? (
-                <a title={project.subtitle} href={project.link}>
-                  {project.title}
-                </a>
-              ) : (
-                <Link title={project.subtitle} to={project.link}>
-                  {project.title}
-                </Link>
-              )}
-            </li>
-          ))}
+          <li className="topnav-list-item">
+            <Link title="Projects that I like to work on" to="/">
+              All Projects
+            </Link>
+          </li>
         </ul>
       </nav>
       <hr />
